8e76f091a05ad3d36ebf05027bad70c6d2ef1d02
Feyza Topgul <feyzatopgul@gmail.com>
2021-10-03 17:54:11 -0700

    Add Contacts code

---
+++ HelloContacts/MyContacts.swift
@@ -0,0 +1,15 @@
+//
+//  Contacts.swift
+//  HelloContacts
+//
+//  Created by fyz on 7/6/18.
+//  Copyright © 2018 Feyza Topgul. All rights reserved.
+//
+
+import Foundation
+struct MyContacts {
+    let firstName: String
+    let familyName: String
+    let phoneNumber: String
+
+}
--- HelloContacts/ViewController.swift
+++ HelloContacts/ViewController.swift
@@ -7,11 +7,37 @@
 //
 
 import UIKit
+import Contacts
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var contactStore = CNContactStore()
+    var contacts = [MyContacts] ()
+    
+    func fetchContacts() {
+        let key = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: key)
+        try! contactStore.enumerateContacts(with: request) { (contact, stoppingPointer) in
+            
+            let firstName = contact.givenName
+            let familyName = contact.familyName
+            let phoneNumber =  contact.phoneNumbers.first?.value.stringValue
+            let contactToAppend = MyContacts (firstName: firstName, familyName: familyName, phoneNumber:phoneNumber!)
+            
+            self.contacts.append(contactToAppend)
+        }
+        
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        contactStore.requestAccess(for: .contacts) { (sucess, error) in
+            if sucess {
+                print("sucessful")
+            }
+        }
+        fetchContacts()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +45,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let contactToDisplay = contacts[indexPath.row]
+        cell.textLabel?.text = contactToDisplay.firstName + " " + contactToDisplay.familyName
+        cell.detailTextLabel?.text =  contactToDisplay.phoneNumber
+        return cell
+        
+    }
+    
 
 
 }
